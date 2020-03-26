e5b8c69c4f37176eb52e3524c00078f8caa1843a
admin <admin@itcast.com>
2020-03-26 20:29:16 +0800

    first commit

---
+++ git-first-project/src/it/yi/gitfist.java
@@ -0,0 +1,10 @@
+package it.yi;
+
+public class gitfist {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+    }
+}
