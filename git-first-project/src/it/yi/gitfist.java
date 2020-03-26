95883126329dd677d0340ec169856701cc36a8c0
admin <admin@itcast.com>
2020-03-26 20:45:00 +0800

    serd

--- git-first-project/src/it/yi/gitfist.java
+++ git-first-project/src/it/yi/gitfist.java
@@ -6,10 +6,12 @@ public class gitfist {
         System.out.println("hello git");
         System.out.println("hello git");
         System.out.println("hello git");
+        System.out.println("111111");
     }
     public void test(){
         int c;
         c=10/2;
         System.out.println(c);
+        //check
     }
 }
